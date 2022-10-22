9549790fb45ea53e2d2919ccb11e61f3959f805d
Jonathan Kuhn <jkuhn96@gmail.com>
2022-10-22 03:29:46 -0400

    Added Planet basic feature.

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -3,9 +3,21 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const Planets = () => {
-    const { id }
+    const { id } = useParams()
+    const [planet, setPlanet] = useState({})
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/planets/${id}`)
+        .then(response=>{setPlanet(response.data)})
+    }, [id])
+
     return (
-        <div>Planets</div>
+        <div>
+            <h2 className='mb-4 display-3'>{planet.name}</h2>
+            <p><strong>climate: </strong>{planet.climate}</p>
+            <p><strong>Terrain: </strong>{planet.terrain}</p>
+            <p><strong>Surface Water: </strong>{planet.surface_water}</p>
+            <p><strong>Population: </strong>{planet.population}</p>
+        </div>
     )
 }
 
