bc1b544fd7786a2be24ffe98bd39f07e3bed4802
Jonathan Kuhn <jkuhn96@gmail.com>
2022-10-21 21:30:02 -0400

    Added state and change functionality to Search.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import People from './components/People'
 import Planets from './components/Planets'
@@ -9,8 +9,8 @@ import Search from './components/Search';
 function App() {
     return (
         <Container className='mt-5'>
-          <Search />
           <BrowserRouter> 
+          <Search />
             <Routes>
               <Route path='/people/:id' element={<People />} />
               <Route path='/planets/:id' element={<Planets />} />
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import  React,{ useState } from 'react'
 import { Form,Button,Row,Col } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
+    const navigate = useNavigate()
+    const [search, setSearch] = useState('')
+    const [id, setId] = useState(0)
+
+    const handleSearchChange = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const handleIdChange = (e) => {
+        setId(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate(`/${search}/${id}`)
+    }
+
   return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Row>
                 <Col sm={7}>
-                    <Form.Group as={Row} controlId='formSearchTypeSelect'>
+                    <Form.Group as={Row} controlId='formSearchSelect' className='mb-5'>
                         <Form.Label column sm={3}>Search for:</Form.Label>
                         <Col sm={9}>
-                            <Form.Select aria-label='Search for'>
-                                <option value='people'>people</option>
-                                <option value='planets'>planets</option>
+                            <Form.Select aria-label='Search for' onChange={handleSearchChange}>
+                                <option value='people'
+                                selected={search === 'people'? true : false}
+                                >people</option>
+                                <option value='planets'
+                                selected={search === 'planets'? true : false}
+                                >planets</option>
                             </Form.Select>
                         </Col>
                     </Form.Group>
@@ -20,7 +42,7 @@ const Search = () => {
                     <Form.Group as={Row} controldID='formSearchIdInput'>
                         <Form.Label column sm={2}>ID:</Form.Label>
                         <Col sm={9}>
-                            <Form.Control type='number' />
+                            <Form.Control type='number' onChange={handleIdChange} />
                         </Col>
                     </Form.Group>
                 </Col>
